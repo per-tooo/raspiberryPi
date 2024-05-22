bf601ee5eecd52a7c78cfdc1ac191ff1a1f88aae
per-tooo <84619185+per-tooo@users.noreply.github.com>
2024-05-22 19:38:49 +0200

    Update standardSetup.sh

--- standardSetup.sh
+++ standardSetup.sh
@@ -21,7 +21,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin -y
 sudo docker run hello-world
 
 sudo docker volume create portainer_data
