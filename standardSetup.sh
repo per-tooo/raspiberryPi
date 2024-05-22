fb87a1a7bf522a3f7ef880b8362f36ce3fb85af0
per-tooo <84619185+per-tooo@users.noreply.github.com>
2024-05-22 18:51:48 +0200

    Create standardSetup.sh

---
+++ standardSetup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+#? install qol packages
+sudo apt install mc htop nano -y
+
+#? docker install
+#  https://docs.docker.com/engine/install/debian/
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
+  sudo apt-get remove $pkg
+done
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo docker run hello-world
